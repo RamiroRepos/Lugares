8472ace94472c21cf9db9e54acf65f1f80cfe316
iLucaru <luis98ccc@gmail.com>
2022-06-09 20:58:42 -0600

    Semana 4

--- app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/LugarFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lugares.R
+import com.lugares.adapter.LugarAdapter
 import com.lugares.databinding.FragmentLugarBinding
 import com.lugares.viewmodel.LugarViewModel
 
@@ -29,6 +32,19 @@ class LugarFragment : Fragment() {
         //Se programa la accion para pasarse a AddLugar
         binding.addLugarBtn.setOnClickListener{
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+        //Activar el Reciclador -RecyclerView
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+
+        lugarViewModel.getAllData.observe(viewLifecycleOwner){
+            lugares -> lugarAdapter.setData(lugares)
         }
 
         return binding.root
